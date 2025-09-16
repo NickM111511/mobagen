bd5a8e40999fbcd8815c45cbca9696c0f0963477
nicholas mastrangelo <nicholas.mastrangelo@mymail.champlain.edu>
2025-09-16 14:21:18 -0400

    started about half of it will finish rest later

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,65 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  int maxSize = world.SideSize();
+  int neighborCount[maxSize][maxSize];
+
+  for (int i = 0; i < maxSize; i++) {
+    for (int j = 0; j < maxSize; j++) {
+      neighborCount[i][j] = 0;
+    }
+  }
+
+  for (int line = 0; line < maxSize; line++) {
+    for (int column = 0; column < maxSize; column++) {
+      Point2D currentTile(line, column);
+
+      if (world.Get(currentTile) == true) {
+        for (int i = -1; i <= 1; i++) {
+          for (int j = -1; j <= 1; j++) {
+            if (!(i == 0 && j == 0)) {
+              Point2D checkTile(line + i, column + j);
+              if (line + i < 0) {
+                if (column + j < 0) {
+                  neighborCount[maxSize - 1][maxSize - 1] += 1; //CountNeighbors(world, checkTile);
+                } else {
+                  neighborCount[maxSize - 1][(column + j) % maxSize] += 1; //CountNeighbors(world, checkTile);
+                }
+              }
+              else if (column + j < 0) {
+                neighborCount[(maxSize + j) % maxSize][maxSize - 1] += 1; //CountNeighbors(world, checkTile);
+              }
+              else {
+                neighborCount[(maxSize + j) % maxSize][(column + j) % maxSize] += 1; //CountNeighbors(world, checkTile);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for (int i = 0; i < maxSize; i++) {
+    for (int j = 0; j < maxSize; j++) {
+      if (neighborCount[i][j] == 0 || neighborCount[i][j] == 1) {
+        world.SetNext(Point2D(i, j), false);
+      }
+      else if (neighborCount[i][j] == 2) {
+        continue;
+      }
+      else if (neighborCount[i][j] == 3) {
+        world.SetNext(Point2D(i, j), true);
+      }
+      else {
+        world.SetNext(Point2D(i, j), false);
+      }
+    }
+  }
+
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  return 1;
 }
