948d0e0bd0addddd4a7c56b9ff41fce056b70fc4
Nicholas Mastrangelo <nickmasterjello13@gmail.com>
2025-09-24 02:24:28 -0400

    i think i have the ouline of the process now just need to fix it.

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,15 +4,28 @@
 #include <climits>
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
-  std::vector<Point2D> nearbyNeighbors = getVisitables(w, randomStartPoint(w));
-  if (nearbyNeighbors.size() > 1) {
-    // get a random neighbor
-  } else if (nearbyNeighbors.size() == 1) {
-    // go to the next neighbor
-  } else {
-    // start to backtrack
+    // check if search has started
+    if(stack.empty() && !visited[0][0]) {
+        stack.emplace_back(0,0);
+      visited[0 - (w->GetSize()/2)][0 - (w->GetSize()/2)] = true;
+      w->SetNodeColor(Point2D(0 - (w->GetSize()/2),0 - (w->GetSize()/2)), Color32(255,3,3));
+    }
+  std::vector<Point2D> nearbyNeighbors = getVisitables(w, stack.back());
+
+
+  if (nearbyNeighbors.size() > 1) { // more than one neighbor, randomly add one
+    visited[nearbyNeighbors[0].y - (w->GetSize()/2)][nearbyNeighbors[0].x - (w->GetSize()/2)] = true;
+    stack.push_back(nearbyNeighbors[0]);
+    w->SetNodeColor(Point2D(nearbyNeighbors[0].x - (w->GetSize()/2),nearbyNeighbors[0].y - (w->GetSize()/2)), Color32(255,3,3));
+  } else if (nearbyNeighbors.size() == 1) { // add the one neighbor
+    visited[nearbyNeighbors[0].y - (w->GetSize()/2)][nearbyNeighbors[0].x - (w->GetSize()/2)] = true;
+    stack.push_back(nearbyNeighbors[0]);
+    w->SetNodeColor(Point2D(nearbyNeighbors[0].x - (w->GetSize()/2),nearbyNeighbors[0].y - (w->GetSize()/2)), Color32(255,3,3));
+  } else { // no neighbors, start to backtrack
+
   }
-  return false;
+
+  return true;
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
@@ -44,7 +57,17 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   // todo: implement this
   for (int i = -1; i < 2; i++) {
     for (int j = -1; j < 2; j++) {
-      // do something here. 
+      if((i == 0 && j == 0) || (i == 1 && j == 1) || (i == -1 && j == 1) || (i == 1 && j == -1) || (i == -1 && j == -1)) { // makes sure it does not include itself as neighbor
+        continue;
+      } else { // checks if any of the neighbors would be out of bounds.
+        if(p.x + j >= 0 && p.x + j <= w->GetSize()-1 && p.y + i >= 0 && p.y + i <= w->GetSize()-1) {
+          if(visited[(p.x + j - sideOver2)][(p.y + i - sideOver2)]) {
+            continue;
+          } else {
+            visitables.emplace_back(p.x + j, p.y + i);
+          }
+        }
+      }
     }
   }
 
